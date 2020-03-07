7775258c874d652302b66ed852246da09726bd98
jorge diaz <jorg1908@hotmail.com>
2020-03-06 21:05:38 -0500

    Subiendo app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { CarListComponent } from './car-list/car-list.component';
 import { CarEditComponent } from './car-edit/car-edit.component';
 
+import { OwnerListComponent } from './owner-list/owner-list.component';
+import { OwnerEditComponent } from './owner-edit/owner-edit.component';
+
 const routes: Routes = [
   { path: '', redirectTo: '/car-list', pathMatch: 'full' },
   {
@@ -16,6 +19,18 @@ const routes: Routes = [
   {
     path: 'car-edit/:id',
     component: CarEditComponent
+  },
+  {
+    path: 'owner-list',
+    component: OwnerListComponent
+  },
+  {
+    path: 'owner-add',
+    component: OwnerEditComponent
+  },
+  {
+    path: 'owner-edit/:id',
+    component: OwnerEditComponent
   }
 ];
 
